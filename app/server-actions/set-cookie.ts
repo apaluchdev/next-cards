6e1390a074a63fff136c71b5daa0ebf34ff38185
apaluchdev <apaluchdev@gmail.com>
2024-08-11 20:46:48 -0400

    Set the same JWT cookie for both domains

--- app/server-actions/set-cookie.ts
+++ app/server-actions/set-cookie.ts
@@ -4,4 +4,5 @@ import { cookies } from "next/headers";
 
 export async function SetCookie(name: string, value: string, expiresDate: Date): Promise<void> {
   cookies().set(name, value, { expires: expiresDate, domain: process.env.NEXT_PUBLIC_GO_BACKEND });
+  cookies().set(name, value, { expires: expiresDate });
 }
