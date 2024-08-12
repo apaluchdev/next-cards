ee865ff57b318342014c44849e02077c3913d383
apaluchdev <apaluchdev@gmail.com>
2024-08-11 20:41:40 -0400

    Updated cookie to be set on specified backend domain

--- app/server-actions/set-cookie.ts
+++ app/server-actions/set-cookie.ts
@@ -3,5 +3,5 @@
 import { cookies } from "next/headers";
 
 export async function SetCookie(name: string, value: string, expiresDate: Date): Promise<void> {
-    cookies().set(name, value, { expires: expiresDate });
+  cookies().set(name, value, { expires: expiresDate, domain: process.env.NEXT_PUBLIC_GO_BACKEND });
 }
